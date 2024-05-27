878de82ebcd5680055abc42277b81cdb88f5fbe1
Mayesha <mim2007088@stud.kuet.ac.bd>
2024-05-27 21:06:37 +0600

    sidebar bug fixed

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -3,16 +3,17 @@
   <div class="form-inline mr-auto"></div>
   <ul class="navbar-nav navbar-right">
     <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-      <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-      <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
+      {{-- <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1"> --}}
+      {{-- <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div> --}}
+    </a>
       <div class="dropdown-menu dropdown-menu-right">
-        <div class="dropdown-title">Logged in 5 min ago</div>
-        <a href="{{route('profile.edit')}}" class="dropdown-item has-icon">
+        {{-- <div class="dropdown-title">Logged in 5 min ago</div> --}}
+        {{-- <a href="{{route('profile.edit')}}" class="dropdown-item has-icon">
           <i class="far fa-user"></i> Profile
         </a>
         <a href="features-settings.html" class="dropdown-item has-icon">
           <i class="fas fa-cog"></i> Settings
-        </a>
+        </a> --}}
         <div class="dropdown-divider"></div>
         <form method="POST" action="{{ route('logout') }}">
             @csrf
@@ -30,7 +31,7 @@
 <div class="main-sidebar sidebar-style-2">
   <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      <a href="index.html">Backend</a>
+      <a href="index.html"></a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
       <a href="index.html">St</a>
